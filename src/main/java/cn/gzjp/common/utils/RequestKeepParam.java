937f8b8cec8e65da8f8ff30e49642adbfa367ed5
hzying19 <hzying19@gmail.com>
2015-04-16 16:05:16 +0800

    Change-Id: I5c6a720a21b7907316d5663ba80933ee367c47cd Signed-off-by: hzying19 <hzying19@gmail.com>

---
+++ src/main/java/cn/gzjp/common/utils/RequestKeepParam.java
@@ -0,0 +1,21 @@
+package cn.gzjp.common.utils;
+
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * HttpServletRequest 保持param
+ * @Description: TODO
+ * @ClassName: RequestKeepParam 
+ * @author huangzy@gzjp.cn
+ * @date 2015年4月16日 下午4:02:21
+ */
+public class RequestKeepParam {
+	public static void keep(HttpServletRequest request,String ...paramNames){
+		if(paramNames!=null&&paramNames.length>0){
+			for(String param:paramNames){
+				String tmp = request.getParameter(param);
+				request.setAttribute(param, tmp);
+			}
+		}
+	}
+}
